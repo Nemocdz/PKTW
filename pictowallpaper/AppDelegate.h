dd73e66fa2f067be59bdc7118caf86a1ccf480fb
Nemocdz <cdz1996@qq.com>
2016-03-06 22:17:06 +0800

    创建应用

---
+++ pictowallpaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pictowallpaper
+//
+//  Created by Nemocdz on 16/3/6.
+//  Copyright © 2016年 Nemocdz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pictowallpaper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pictowallpaper
+//
+//  Created by Nemocdz on 16/3/6.
+//  Copyright © 2016年 Nemocdz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
